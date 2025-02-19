400a43d253b6df5816d7319f51c43bd51fde05a9
Javoxir <javokhirabdurashitov@gmail.com>
2025-02-19 22:59:43 +0500

    categories section made in css (but there is some issue when you press show more button items lose its shape)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-// $(document).ready(function(){
-//     $('.category-toggle').click(function(){
-//         $('.hide-category').toggleClass('d-none');
-//         $('.show-more').toggleClass('d-none');
-//         $('.show-less').toggleClass('d-none');
-//     });
-// });
+$(document).ready(function () {
+    $(".hide-category").hide(); // Hide 19 categories initially
+
+    $(".category-toggle").click(function () {
+        if ($(".hide-category").is(":visible")) {
+            // Hide extra categories
+            $(".hide-category").slideUp();
+            $(".show-more").show();
+            $(".show-less").hide();
+        } else {
+            // Show all categories
+            $(".hide-category").slideDown();
+            $(".show-more").hide();
+            $(".show-less").show();
+        }
+    });
+});
