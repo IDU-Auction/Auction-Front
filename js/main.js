59ef65aa17c4679cea01cf45ccd4ee7df4885498
Javoxir <javokhirabdurashitov@gmail.com>
2025-02-23 23:32:40 +0500

    Help section added, but there are some bugs must be fixed, swiper buttons  work not correctly t affcts to the Advertising banners

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 });
 
 
-// Swiper script
+// Swiper script \\
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -25,9 +25,23 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
   });
+// /Swiper script \\
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
